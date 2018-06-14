91050e23f8a396035ba1752dfc91678f5d5350e2
Tony Gaddy <ragaddy@richmondcc.edu>
2018-06-14 16:00:01 -0400

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -3,15 +3,6 @@ let socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  // socket.emit('createEmail', {
-  //   to: 'jen@example.com',
-  //   text: 'Hey. This is Andrew'
-  // });
-
-  // socket.emit('createMessage', {
-  //   from: 'Client',
-  //   text: 'Hello from client!!'
-  // });
 });
 
 socket.on('disconnect', function() {
@@ -21,7 +12,3 @@ socket.on('disconnect', function() {
 socket.on('newMessage', function (message) {
   console.log('newMessage: ', message);
 });
-
-// socket.on('newEmail', function (email) {
-//   console.log('New Email', email);
-// });
