0baf351703e9569c89dc264e59d3c3b72a119c56
Tony Gaddy <ragaddy@richmondcc.edu>
2018-06-18 11:25:57 -0400

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
-  socket.emit('createMessage', {
-    from: 'User',
-    text: jQuery('[name=message]').val()
-  }, function () {
+  let messageTextbox = jQuery('[name=message]');
 
+  socket.emit('createMessage', {
+    text: messageTextbox.val(),
+    from: 'User'
+  }, function () {
+    messageTextbox.val('');
   });
 });
 
@@ -42,13 +44,17 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
     console.log(position);
   }, function () {
-    alert('Unable to fetch location')
+    alert('Unable to fetch location');
+    locationButton.removeAttr('disabled').text('Send location');
   });
 });
